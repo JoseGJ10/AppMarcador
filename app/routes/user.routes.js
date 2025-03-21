09d86c710c0f7ee81d4b4d548f7c426d13cfe82f
Jose Luis Garcia Jimenez <jose.garciajimenez@gmail.com>
2025-03-21 06:44:22 +0100

    se cambia ruta create user

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -8,6 +8,6 @@ router.get('/userByid', userController.getUserById);
 
 router.get('/userByname', userController.getUserByUsername);
 
-router.post('/users', userController.createUser);
+router.post('/createUser', userController.createUser);
 
 module.exports = router
