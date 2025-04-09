b32b59d1f405a40e37d9528c10a264fd7f5ada94
Jose Luis Garcia Jimenez <jose.garciajimenez@gmail.com>
2025-04-09 12:22:20 +0200

    cambios index routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,23 @@
 const authRoutes = require("./auth.routes.js");
-const boardGame = require("./boardGame.routes.js");
+const boardGameRoutes = require("./boardGame.routes.js");
+const eventParticipantRoutes = require("./event_participant.routes.js");
+const eventRoutes = require("./event.routes.js");
 const gameRoutes = require("./game.routes.js");
+const gamePlayerRoutes = require("./gamePlayer.routes.js");
 const homeRoutes = require("./home.routes.js");
-const userRoutes = require("./user.routes.js");
-const roleRoutes = require('./role.routes.js');
 const loanRoutes = require("./loan.routes.js");
+const roleRoutes = require('./role.routes.js');
+const userRoutes = require("./user.routes.js");
 
 module.exports = {
-    authRoutes,  
-    boardGame,
-    gameRoutes,  
+    authRoutes,
+    boardGameRoutes,
+    eventParticipantRoutes,
+    eventRoutes,
+    gameRoutes,
+    gamePlayerRoutes,
     homeRoutes,
-    userRoutes,
+    loanRoutes,
     roleRoutes,
-    loanRoutes
+    userRoutes,
  };
