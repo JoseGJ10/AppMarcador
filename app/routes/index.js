e401d731d00d866f314a7ae8105d65b51624372d
Jose Luis Garcia Jimenez <jose.garciajimenez@gmail.com>
2025-06-01 21:52:25 +0200

    rutas de contact

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 const authRoutes = require("./auth.routes.js");
 const boardGameRoutes = require("./boardGame.routes.js");
+const contactRoutes = require('./contact.route.js')
 const eventParticipantRoutes = require("./event_participant.routes.js");
 const eventRoutes = require("./event.routes.js");
 const homeRoutes = require("./home.routes.js");
@@ -8,10 +9,10 @@ const matchRoutes = require("./match.routes.js");
 const participantRoutes = require("./participant.routes.js");
 const roleRoutes = require('./role.routes.js');
 const userRoutes = require("./user.routes.js");
-
 module.exports = {
     authRoutes,
     boardGameRoutes,
+    contactRoutes,
     eventParticipantRoutes,
     eventRoutes,
     homeRoutes,
